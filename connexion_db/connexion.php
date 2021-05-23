a1170e93a710a468db6eb165ce17dd2856540fca
Lucas <lucrod1000@gmail.com>
2021-05-23 23:05:37 +0200

    Update connexion.php

--- connexion_db/connexion.php
+++ connexion_db/connexion.php
@@ -3,7 +3,7 @@
 // fonction de connexion à la base de donnée
 
 function connect(){
-    $connexion = mysqli_connect ("localhost","root","","io2_projet") ;
+    $connexion = mysqli_connect ("localhost","root","","IO2_PROJET_GROUPE_1") ;
      
      if (!$connexion) {
          exit ;
